8cc3f3254014b4d1449fe65a0c0c1d907d97ae21
raizengxd <rickdavidsangalang@gmail.com>
2020-06-27 16:24:37 +0800

    add

--- prototayp/lib/main.dart
+++ prototayp/lib/main.dart
@@ -1,17 +1,18 @@
 import 'package:flutter/material.dart';
-import 'package:percent_indicator/percent_indicator.dart';
+import 'theOtherCard.dart';
+import 'theCard.dart';
 
 void main() => runApp(MyApp());
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-    home: MyPage(),
-    
-    
+      home: MyPage(),
     );
   }
 }
+
 class MyPage extends StatefulWidget {
   @override
   _MyPageState createState() => _MyPageState();
@@ -23,18 +24,19 @@ class _MyPageState extends State<MyPage> {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.blue,
-        title: Center(child: Text('Habit Tracker')),       
-        
+        title: Center(child: Text('Habit Tracker')),
       ),
-      body:Column(children: <Widget>[
-        FlatButton(onPressed: (){}, child: Text("dfd")),
-        Flexible(child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: ADDTile(),
-        ))
-      ],),
-      );
-    
+      body: Column(
+        children: <Widget>[
+          FlatButton(onPressed: () {}, child: Text("dfd")),
+          Flexible(
+              child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: ADDTile(),
+          ))
+        ],
+      ),
+    );
   }
 }
 
@@ -47,51 +49,12 @@ class _ADDTileState extends State<ADDTile> {
   @override
   Widget build(BuildContext context) {
     return ListView(
-  children: <Widget>[
-    
-    TheCards("dfdfd",Colors.red,"Read Book"),
-    TheCards("dfdfdwe",Colors.blue,"Maligo"),
-    TheCards("dfdfd",Colors.green,"Read Book"),
-    TheCards("dfdfdwe",Colors.purple,"Maligo"),
-    TheCards("dfdfd",Colors.pink,"Read Book"),
-    TheCards("dfdfdwe",Colors.blue,"Maligo"),
-    TheCards("dfdfd",Colors.red,"Read Book"),
-    TheCards("dfdfdwe",Colors.green,"Maligo"),
-    TheCards("dfdfd",Colors.red,"Read Book"),
-    TheCards("dfdfdwe",Colors.blue,"Maligo"),
-    TheCards("dfdfd",Colors.red,"Read Book"),
-    TheCards("dfdfdwe",Colors.blue,"Maligo"),
-
-
-   
-   
-  ],
-);
-  }
-}
-class TheCards extends StatelessWidget {
-  final String name;
-  final Color color;
-  final String habitName;
-  TheCards(this.name, this.color, this.habitName);
-  @override
-  Widget build(BuildContext context) {
-    return
-     Card(child: ListTile(subtitle: Text("dfdfdkfkdkdfer"),leading: Icon(Icons.image),title: new LinearPercentIndicator(
-      width: 170.0,
-                animation: true,
-                animationDuration: 1000,
-                lineHeight: 20.0,
-                leading: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Text(habitName),
-                ),
-               
-                percent: 0.7565645,
-                center: Text("5.0%"),
-                linearStrokeCap: LinearStrokeCap.roundAll,
-                progressColor: color,
-    ),
-     ));
+      children: <Widget>[
+        PercentageCard("dfdfd", Colors.red, "Read Book"),
+        TheCards("dfdfd", Colors.red, "Read Book"),
+        TheCards("dfdfdwe", Colors.blue, "Maligo"),
+        TheCards("dfdfd", Colors.green, "Program for Structure"),
+      ],
+    );
   }
 }
---
+++ prototayp/lib/theOtherCard.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:percent_indicator/percent_indicator.dart';
+
+class PercentageCard extends StatelessWidget {
+  final String name;
+  final Color color;
+  final String habitName;
+  PercentageCard(this.name, this.color, this.habitName);
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.all(15.0),
+      child: new LinearPercentIndicator(
+        width: MediaQuery.of(context).size.width - 90,
+        animation: true,
+        lineHeight: 50.0,
+        animationDuration: 2000,
+        percent: 0.9,
+        center: Text(habitName),
+        linearStrokeCap: LinearStrokeCap.butt,
+        progressColor: Colors.greenAccent,
+      ),
+    );
+  }
+}
