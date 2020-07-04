0c3d6dae3d199babaa94ab426d3c369f7f751d47
raizengxd <rickdavidsangalang@gmail.com>
2020-07-04 23:24:33 +0800

    testing QRCode

--- prototayp/lib/page/qrreader/qrreader.dart
+++ prototayp/lib/page/qrreader/qrreader.dart
@@ -1,19 +1,36 @@
 import 'package:flutter/material.dart';
 
+import 'package:qrscan/qrscan.dart' as scanner;
+
+TextEditingController outputController;
+
 class QRReaderPage extends StatefulWidget {
   @override
   _QRReaderPageState createState() => _QRReaderPageState();
 }
 
 class _QRReaderPageState extends State<QRReaderPage> {
+  var outputController = new TextEditingController();
+  Future _scan() async {
+    String barcode = await scanner.scan();
+    outputController.text = barcode;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
-        child: Center(
-      child: Text(
-        "QR Code cs",
-        style: TextStyle(color: Colors.red),
+      child: Column(
+        children: [
+          FlatButton(
+            child: Text("Scann"),
+            color: Colors.blue,
+            onPressed: () {
+              _scan();
+            },
+          ),
+          Container(child: Text(outputController.text)),
+        ],
       ),
-    ));
+    );
   }
 }
