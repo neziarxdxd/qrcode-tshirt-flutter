f97c3d156cfea2cbe9cd5580d570f9dc13af53af
raizengxd <rickdavidsangalang@gmail.com>
2020-07-19 22:46:41 +0800

    add quotes

--- prototayp/lib/page/summary/apiQuotes.dart
+++ prototayp/lib/page/summary/apiQuotes.dart
@@ -1,15 +1,1 @@
-import 'dart:async';
-import 'dart:convert';
-import 'package:http/http.dart' as http;
-
-class APIQuotes {
-  Map data;
-  List userData;
-  Future getData() async {
-    final url =
-        'https://raw.githubusercontent.com/zenraiching/web-practice/master/motivate.json';
-    http.Response response = await http.get(url);
-    data = json.decode(response.body);
-    userData = data["data"];
-  }
-}
+class APIQuotes {}
