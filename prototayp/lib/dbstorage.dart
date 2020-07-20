edbdaa6f3f7d99d32aa0c68505b109b40ce64d49
raizengxd <rickdavidsangalang@gmail.com>
2020-07-20 18:46:04 +0800

    remove profession variable

--- prototayp/lib/dbstorage.dart
+++ prototayp/lib/dbstorage.dart
@@ -6,7 +6,6 @@ final String tablePerson = "tblPerson";
 final String personID = "id";
 final String personName = "name";
 final String personBio = "bio";
-final String personProfession = "profession";
 final String personfavoritePL = "favorite_programminglanguage";
 final String personTellMeMore = "tell_more";
 
@@ -14,24 +13,17 @@ class PersonModel {
   int pId;
   final String pName;
   final String pBio;
-  final String pProfession;
+
   final String pFavoritePL;
   final String pTellMeMore;
 
   PersonModel(
-      {this.pId,
-      this.pName,
-      this.pBio,
-      this.pProfession,
-      this.pFavoritePL,
-      this.pTellMeMore});
+      {this.pId, this.pName, this.pBio, this.pFavoritePL, this.pTellMeMore});
 
   Map<String, dynamic> toMap() {
     return {
       personName: this.pName,
-      personProfession: this.pProfession,
       personBio: this.pBio,
-      personProfession: this.pProfession,
       personfavoritePL: this.pFavoritePL,
       personTellMeMore: this.pTellMeMore
     };
@@ -51,7 +43,6 @@ class PersonHelper {
       return db.execute('''
       CREATE TABLE $tablePerson ($personID INTEGER PRIMARY KEY AUTOINCREMENT,
           $personName TEXT,
-          $personProfession TEXT,
           $personBio TEXT,
           $personfavoritePL TEXT,
           $personTellMeMore TEXT
@@ -80,7 +71,6 @@ class PersonHelper {
           pId: tasks[index][personID],
           pName: tasks[index][personName],
           pBio: tasks[index][personBio],
-          pProfession: tasks[index][personProfession],
           pFavoritePL: tasks[index][personfavoritePL],
           pTellMeMore: tasks[index][personTellMeMore]);
     });
